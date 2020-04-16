40f54cb8302b253fb7081d9913c216c8dc93bbce
Laura Molina <57803838+lausvision@users.noreply.github.com>
2020-04-16 10:34:09 +0200

    templete created

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+// ARRAY WITH CHATBOT REPLIES (STRINGS)
+const replies = [
+  "Hello, Welcome to IronChat!",
+  "Ironhack's coding LAB a day keeps the bugs away!",
+  "Thank you for your visit. See you soon!",
+  "Goodbye!",
+  "Nooo way! Same here!!!",
+  "How does that feel on a scale from 1 to 10 ?",
+  "Hmmmm ... interesting!",
+  "That could be a problem.",
+  "Do not go to the bathroom in a dream. It’s a trap!",
+  "It does make sense.",
+  "Why is that?",
+  "Who?"
+];
+
+// GET THE DOM ELEMENTS
+
+// HELPER FUNCTIONS
+function sendMessage() {}
+
+function chatbotReply() {}
+
+function displayMessage() {}
+
+// EVENT LISTENERS
